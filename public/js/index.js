899505ed9343044491c69bc823208a0d3a58469c
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2019-01-29 20:09:28 +0200

    Tt

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+let socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
+
+// socket.emit('createMessage', {
+//     name: 'Dkight',
+//     text: 'Hey!'
+// });
--- server/server.js
+++ server/server.js
@@ -12,12 +12,35 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
 
     socket.on('disconnect', () => {
         console.log('Client has been disconnected');
     });
-    
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Hello from admin',
+        createdAt: Date.now()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user is joined',
+        createdAt: Date.now()
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: Date.now()
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: 'Dima',
+        //     text: 'Hello again!',
+        //     createdAt: 123432432
+        // });
+    });
 });
 
 
