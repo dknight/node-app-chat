5a1a11cdd01aa1aefd868ec33ae9788fc0311e31
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2019-01-29 19:33:40 +0200

    basic sockets

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const APP_PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-const server = app.listen(APP_PORT, () => {
+    socket.on('disconnect', () => {
+        console.log('Client has been disconnected');
+    });
+    
+});
+
+
+server.listen(APP_PORT, () => {
     console.log(`Listening on port ${APP_PORT}`);
-})
+});
